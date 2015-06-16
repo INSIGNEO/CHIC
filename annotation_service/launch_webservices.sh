8b38d231719c250ef712b68acd3351508115664d
Nicolas Gruel <nicolas.gruel@gmail.com>
2015-06-16 23:53:13 +0100

    Update annotations service to the last production instance found on CSE server and known to work.

--- annotation_service/launch_webservices.sh
+++ annotation_service/launch_webservices.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd  /home/tartarini/PRODUCTION/annotation_service/
+ANNOTATIONS=/home/vagrant/CHIC-services/annotations/annotation_service/
+cd  $ANNOTATIONS
 
-sudo python /home/tartarini/PRODUCTION/annotation_service/rest_chic_annotation.py 80  2>&1 | tee log_80.txt  &
-sudo python /home/tartarini/PRODUCTION/annotation_service/rest_chic_annotation_8090.py 8090  2>&1 | tee log_8090.txt  &
-sudo python /home/tartarini/PRODUCTION/annotation_service/rest_chic_annotation_8091.py 8091  2>&1 | tee log_8091.txt  &
+sudo python $ANNOTATIONS/rest_chic_annotation_8090.py 8090  2>&1 | tee log_8090.txt  &
+sudo python $ANNOTATIONS/rest_chic_annotation_8091.py 8091  2>&1 | tee log_8091.txt  &
 
 cd -
--- annotation_service/rest_chic_annotation_test_8099.py
+++ annotation_service/rest_chic_annotation_test_8099.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as ET
 
 from time import gmtime, strftime
 
-resource_file = 'Dawn_model_resource_8091.xml'
+resource_file = 'Dawn_model_resource_8099.xml'
 tree = ET.parse(resource_file)
 root = tree.getroot()
 
@@ -77,7 +77,7 @@ class get_resource_id_metadata_by_name:
   def GET(self, resource_id, metadata_name):
     web.header('Content-Type', 'text/xml');
     print('get_resource_id_metadata_by_name');
-    print("getting resourceID="+str(resource_id)+" metadata="+str(metadata_name)+'content_type='+str(http_content_type))
+    print "getting resourceID="+str(resource_id)+" metadata="+str(metadata_name)+'content_type='+str(http_content_type);
     for child in root:
       if child.attrib['resourceid'] == str(resource_id):
         resource = child;
@@ -114,7 +114,7 @@ class get_resource_id_metadata_by_name:
             xml_element =xml_element_specific;
           
           
-          print('output='+str(output))
+          print 'output='+str(output);
           
           if http_content_type != 'xml':
             output == xml_element.text;
@@ -139,7 +139,7 @@ class get_resource_id_metadata_by_name:
 class get_resource_id_specific_metadata_by_name:
   def GET(self, resource_id, metadata_name):
     print('get_resource_id_specific_metadata_by_name');
-    print("getting resourceID="+str(resource_id)+" metadata="+str(metadata_name)+'content_type='+str(http_content_type))
+    print "getting resourceID="+str(resource_id)+" metadata="+str(metadata_name)+'content_type='+str(http_content_type);
     for child in root:
       if child.attrib['resourceid'] == str(resource_id):
         resource = child;
@@ -167,7 +167,7 @@ class get_resource_id_specific_metadata_by_name:
         try:
           xml_element = resource_specific_metadata.find(metadata_name);
           output = ET.tostring(xml_element, 'utf-8', method='xml')
-          print('output='+str(output))
+          print 'output='+str(output);
 
           if http_content_type != 'xml':
             output == xml_element.text;
@@ -192,7 +192,7 @@ class get_resource_id_common_metadata_by_name:
   def GET(self, resource_id, metadata_name):
     web.header('Content-Type', 'text/xml');
     print('get_resource_id_common_metadata_by_name');
-    print("getting resourceID="+str(resource_id)+" metadata="+str(metadata_name)+'content_type='+str(http_content_type))
+    print "getting resourceID="+str(resource_id)+" metadata="+str(metadata_name)+'content_type='+str(http_content_type);
     for child in root:
       if child.attrib['resourceid'] == str(resource_id):
         resource = child;
@@ -201,7 +201,7 @@ class get_resource_id_common_metadata_by_name:
 
         try:
           output = ET.tostring(xml_element, 'utf-8', method='xml')
-          print('output='+str(output))
+          print 'output='+str(output);
           if http_content_type != 'xml':
             output == xml_element.text;
  
@@ -222,7 +222,7 @@ class get_element_by_name:
   def GET(self, resource_id, element_name):
     web.header('Content-Type', 'text/xml');
     print('get_element_by_name');
-    print("get resource description "+str(resource_id))
+    print "get resource description "+str(resource_id)
     # ET.dump(tree)
     #output = ET.tostring(root, 'utf-8', method='xml')
     
@@ -245,7 +245,7 @@ class get_element_by_name:
 class get_description:
   def GET(self, resource_id):
     print('get_description');
-    print("get resource description "+str(resource_id))
+    print "get resource description "+str(resource_id)
     # ET.dump(tree)
     #output = ET.tostring(root, 'utf-8', method='xml')
 
@@ -269,7 +269,7 @@ class get_resource_xml:
   def GET(self, resource_id):
     web.header('Content-Type', 'text/xml')
     print('get_resource_xml');
-    print("get resource "+str(resource_id))
+    print "get resource "+str(resource_id)
     # ET.dump(tree)
     #output = ET.tostring(root, 'utf-8', method='xml')
     #return output
@@ -320,7 +320,7 @@ class get_model_list:
         models_root = tree.find('Resource')
         output = 'models:[';
         for child in models_root:
-            print('child', child.tag, child.attrib)
+            print 'child', child.tag, child.attrib
             output += str(child.attrib) + str(child.text) +','
         output += ']';
         
@@ -334,7 +334,7 @@ class list_users:
     def GET(self):
         output = 'users:[';
         for child in root:
-            print('child', child.tag, child.attrib)
+            print 'child', child.tag, child.attrib
             output += str(child.attrib) + ','
         output += ']';
         return output
