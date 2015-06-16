0ca8dfb013968586cf2269a655deabcb86f19a3e
Nicolas Gruel <nicolas.gruel@gmail.com>
2015-06-16 23:08:45 +0100

    Add scripts to be used to start automatically the different services needed by VPH-HF. They have been written for vagrant image.

---
+++ Scripts/etc/start_chic_services.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VPHHOME=/home/vagrant
+CHICSERVICES=$VPHHOME/CHIC-services
+
+#Need to use the python from the system.
+# initializing python environment
+source $VPHHOME/py2/bin/activate
+
+echo "Launch CHIC annotation web services"
+python2 $CHICSERVICES/annotations/nnotations_service/rest_chic_annotation_8090.py 8090 2>&1 | tee $VPHHOME/log/annotation_8090  &
+python2 $CHICSERVICES/annotations/annotation_service/rest_chic_annotation_8091.py 8091 2>&1 | tee $VPHHOME/log/annotation_8091  &
+cd -
+
+# initializing python environment
+source $VPHHOME/py3/bin/activate
+
+python3 $CHICSERVICES/repositories/repository/manage.py runserver cse.shef.ac.uk:8010 2>&1 | tee $VPHHOME/log/model_and_datarepo_8010 &
---
+++ Scripts/etc_system/init.d/start_chic_services.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Starting CSE services.."
+
+CHIC_HOME=/home/vagrant
+
+#Start taverna
+su vagrant-c "bash -c '$CHIC_HOME/apache-tomcat-7.0.59/bin/startup.sh'"
+
+#Start VPH-HF services
+su vagrant -c "bash -c $CHIC_HOME/etc/start_vphhf_services.sh"
+
+#Start CHIC services
+su vagrant -c "bash -c $CHIC_HOME/etc/start_chic_services.sh"
+
+
+
