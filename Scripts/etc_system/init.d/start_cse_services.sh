66d5352a7d971add5f916fd63ff5edbbf8693acf
Nicolas Gruel <nicolas.gruel@gmail.com>
2015-06-17 00:55:52 +0100

    Correct the path to start tomcat

--- Scripts/etc_system/init.d/start_cse_services.sh
+++ Scripts/etc_system/init.d/start_cse_services.sh
@@ -5,7 +5,7 @@ echo "Starting CSE services.."
 CHIC_HOME=/home/vagrant
 
 #Start taverna
-su vagrant -c "bash -c '$CHIC_HOME/apache-tomcat-7.0.59/bin/startup.sh'"
+su vagrant -c "bash -c '$CHIC_HOME/taverna/apache-tomcat-7.0.59/bin/startup.sh'"
 
 #Start VPH-HF services
 su vagrant -c "bash -c $CHIC_HOME/etc/start_vphhf_services.sh"
