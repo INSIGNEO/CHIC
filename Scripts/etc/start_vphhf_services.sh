826d3e390932ab9c11411a7b7290afc9b79620a4
Nicolas Gruel <nicolas.gruel@gmail.com>
2015-06-17 13:13:35 +0100

    Remove localhost by 0.0.0.0 in django application starting so Vagrant can forward the port correctly

--- Scripts/etc/start_vphhf_services.sh
+++ Scripts/etc/start_vphhf_services.sh
@@ -6,8 +6,8 @@ VPHHOME=/home/vagrant
 source $VPHHOME/py2/bin/activate
 
 python2 $VPHHOME/VPH-HF/web/sms_manage.py migrate
-python $VPHHOME/VPH-HF/web/sms_manage.py runserver localhost:8000 2>&1 | tee $VPHHOME/log/sms_8000 &
+python2 $VPHHOME/VPH-HF/web/sms_manage.py runserver 0.0.0.0:8000 2>&1 | tee $VPHHOME/log/sms_8000 &
 
 python2 $VPHHOME/VPH-HF/web/director_manage.py migrate
-python $VPHHOME/VPH-HF/web/director_manage.py runserver localhost:8001 2>&1 | tee $VPHHOME/log/director_8001 &
+python2 $VPHHOME/VPH-HF/web/director_manage.py runserver 0.0.0.0:8001 2>&1 | tee $VPHHOME/log/director_8001 &
 
