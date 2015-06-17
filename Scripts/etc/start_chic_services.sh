f2a470853dd5b4ea47e7ef21f195abdc5d6e2570
Nicolas Gruel <nicolas.gruel@gmail.com>
2015-06-17 21:17:31 +0000

    Refactor a little bit the starting scripts

--- Scripts/etc/start_chic_services.sh
+++ Scripts/etc/start_chic_services.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-VPHHOME=/home/vagrant
-CHICSERVICES=$VPHHOME/CHIC-services
+CHICSERVICES=$HOME/CHIC-services
 
 #Need to use the python from the system.
 # initializing python environment
-source $VPHHOME/py2/bin/activate
+source $HOME/py2/bin/activate
 
 echo "Launch CHIC annotation web services"
 cd $CHICSERVICES/annotations/annotation_service/
-python2 $CHICSERVICES/annotations/annotation_service/rest_chic_annotation_8090.py 8090 2>&1 | tee $VPHHOME/log/annotation_8090  &
-python2 $CHICSERVICES/annotations/annotation_service/rest_chic_annotation_8091.py 8091 2>&1 | tee $VPHHOME/log/annotation_8091  &
+python2 rest_chic_annotation_8090.py 8090 2>&1 | tee $HOME/log/annotation_8090  &
+python2 rest_chic_annotation_8091.py 8091 2>&1 | tee $HOME/log/annotation_8091  &
 cd -
 
 # initializing python environment
-source $VPHHOME/py3/bin/activate
+source $HOME/py3/bin/activate
 python3 $CHICSERVICES/repositories/repository/manage.py migrate
-python3 $CHICSERVICES/repositories/repository/manage.py runserver 0.0.0.0:8010 2>&1 | tee $VPHHOME/log/model_and_datarepo_8010 &
+python3 $CHICSERVICES/repositories/repository/manage.py runserver 0.0.0.0:8010 2>&1 | tee $HOME/log/model_and_datarepo_8010 &
--- Scripts/etc/start_vphhf_services.sh
+++ Scripts/etc/start_vphhf_services.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-VPHHOME=/home/vagrant
+VPHHF=/home/vagrant/VPH-HF/web
 
 #Initialize python environment.
-source $VPHHOME/py2/bin/activate
+source $HOME/py2/bin/activate
 
-python2 $VPHHOME/VPH-HF/web/sms_manage.py migrate
-python2 $VPHHOME/VPH-HF/web/sms_manage.py runserver 0.0.0.0:8000 2>&1 | tee $VPHHOME/log/sms_8000 &
+python2 $VPHHF/sms_manage.py migrate
+python2 $VPHHF/sms_manage.py runserver 0.0.0.0:8000 2>&1 | tee $HOME/log/sms_8000 &
 
-python2 $VPHHOME/VPH-HF/web/director_manage.py migrate
-python2 $VPHHOME/VPH-HF/web/director_manage.py runserver 0.0.0.0:8001 2>&1 | tee $VPHHOME/log/director_8001 &
+python2 $VPHHF/director_manage.py migrate
+python2 $VPHHF/director_manage.py runserver 0.0.0.0:8001 2>&1 | tee $HOME/log/director_8001 &
+
+python2 $VPHHF/chic_insilicotrial_mockup/manage.py migrate
+python2 $VPHHF/chic_insilicotrial_mockup/manage.py runserver 0.0.0.0:8002 2>&1 | tee $HOME/log/insilico_8002 &
 
